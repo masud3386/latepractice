23359e61be493c33f152984b36dd73db514ddb46
masud3386 <masud3386@yahoo.com>
2025-02-04 16:08:24 -0500

    this is my second commiting code

---
+++ src/test/java/arraysss/Modificaton.java
@@ -0,0 +1,9 @@
+package arraysss;
+
+public class Modificaton {
+
+	public static void main(String[] args) {
+System.out.println("If you see this statement,it confirm you have succefully modified");
+	}
+
+}
